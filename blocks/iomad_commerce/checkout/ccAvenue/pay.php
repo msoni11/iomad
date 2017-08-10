1777ff6da15a4ce5c8e57f33cceb214a03b223a6
Mahendra Soni <mmahendra.soni@gmail.com>
2017-08-10 15:28:00 +0530

    Make CCAvenue live.

--- blocks/iomad_commerce/checkout/ccAvenue/pay.php
+++ blocks/iomad_commerce/checkout/ccAvenue/pay.php
@@ -45,7 +45,7 @@ $encrypted_data = $ccavenue->getEncryptedData();
 ?>
 
 <!-- Request -->
-<form method="post" name="redirect" action="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction">
+<form method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction">
     <?php
     echo "<input type=hidden name=encRequest value=$encrypted_data>";
     echo "<input type=hidden name=access_code value=$access_code>";
